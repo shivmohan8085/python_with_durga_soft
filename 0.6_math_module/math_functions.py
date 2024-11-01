5b90c963b60f451e12aa1952e9bb0b921f2abcb4
shivmohan sharma <shiv8085official@gmail.com>
2024-11-01 19:37:40 +0530

    add code

---
+++ 0.6_math_module/math_functions.py
@@ -0,0 +1,15 @@
+import math
+print(math.sqrt(16))  # 4.0
+
+
+import math as m
+print(m.pi)  # 3.141592653589793
+
+from math import sqrt , pi
+print(sqrt(256))  # 16.0
+print(pi) #3.141592653589793
+
+# find the area off circle
+# pi*r**2
+r = 16
+print(pi*r**2)  #804.247719318987
---
+++ 0.7_I_O_Statements/i-o_statemets.py
@@ -0,0 +1,6 @@
+# x = raw_input("hii: ") # error
+# x = input()
+# print( type(x))
+
+a = eval(input("Enter first number: "))
+a = eval(input("Enter first number: "))
